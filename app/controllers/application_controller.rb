f9b6f7ddc467ffa280dbf11903118c6feef5306b
Ivan Danci <idanci@nebulent.com>
2013-05-26 02:53:07 +0300

    Redirect after signin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,4 +5,9 @@ class ApplicationController < ActionController::Base
     redirect_to root_path, :alert => exception.message
   end
 
+ def after_sign_in_path_for(resource_or_scope)
+    if request.env['omniauth.origin']
+      request.env['omniauth.origin']
+    end
+  end
 end
