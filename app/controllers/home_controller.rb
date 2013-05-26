506fa93432bec2d804c8780c48dc7ffe4d26e53b
Ivan Danci <idanci@nebulent.com>
2013-05-26 04:23:02 +0300

    Allow downloading files

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,21 @@
 class HomeController < ApplicationController
   def index
+    @refs = generate_links
   end
 
   def registered_users
     @users = User.all
+  end
+
+private
+  def generate_links
+    refs = []
+    Dir[Rails.root.join("public/univer_pub/*")].each do |file|
+      name = file.split('/').last.split('.').first.humanize
+      path = file.split('public').last
+      hashik = {:name => name, :path => path}
+      refs << hashik
+    end
+    refs
   end
 end
