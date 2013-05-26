edcae7350d19edcdff9cb9072583a8b74c485f49
Ivan Danci <idanci@nebulent.com>
2013-05-26 03:02:09 +0300

    Rollback redirect

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,10 +4,4 @@ class ApplicationController < ActionController::Base
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_path, :alert => exception.message
   end
-
- def after_sign_in_path_for(resource_or_scope)
-    if request.env['omniauth.origin']
-      request.env['omniauth.origin']
-    end
-  end
 end
